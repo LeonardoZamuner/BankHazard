97ea80f6c52d57a13fdb5161b0307905d9aa17e5
La sedia di lillà <70888953+LeonardoZamuner@users.noreply.github.com>
2024-04-20 12:56:41 +0200

    Delete BankHazard directory
    Only for the new one

--- BankHazard/Log/Logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    BaseFunction::CreateSession();
-    // elimina le variabili di sessione impostate
-    $_SESSION = array();
-    // elimina la sessione
-    session_destroy();
-    header("Location: Login.php");
-?>
