be8727b965613cee029e859ea17fd0eb0ceec26b
GabibboDiStrisciaLaNotizia <102377676+GabibboDiStrisciaLaNotizia@users.noreply.github.com>
2024-05-11 11:57:38 +0200

    Delete BankHazard directory

--- BankHazard/Pages/investimenti/fondi_comuni/fondi_comuni.js
+++
@@ -1,7 +0,0 @@
-function investiOra(fondo) {
-    alert('Hai scelto di investire nel ' + fondo);
-}
-
-function visualizzaTutti() {
-    alert('Visualizza tutti i fondi comuni disponibili');
-}
--- BankHazard/Pages/pagamenti/bonifico/bonifico.js
+++
@@ -1,5 +0,0 @@
-// Codice JavaScript per la pagina Bonifico
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita il comportamento predefinito del form
-    $('#confirmModal').modal('show'); // Mostra la finestra modale al momento del submit del form
-});
