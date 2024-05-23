d3ca9181b1a6e6548c2edcb46a7bb8fee3ec0ed7
LeonardoZamuner <leonardozamuner.05@gmail.com>
2024-05-23 16:36:59 +0200

    eliminazione spazi inutili

--- BaseFunction/BaseFunction.php
+++ BaseFunction/BaseFunction.php
@@ -1,26 +1,25 @@
 <?php 
-class  BaseFunction{
-    public static function DBconnection() : mysqli{
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "bankhazarddb";
-        $conn = new mysqli($servername, $username, $password, $dbname);
+    class  BaseFunction{
+        public static function DBconnection() : mysqli{
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "bankhazarddb";
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-        if ($conn->connect_error) {
-            die("Connessione al database fallita: " . $conn->connect_error);
+            if ($conn->connect_error) {
+                die("Connessione al database fallita: " . $conn->connect_error);
+            }
+            return $conn;
         }
-        return $conn;
-    }
 
-    public static function CreateSession() : bool {
-        session_save_path();
-        return session_start();
+        public static function CreateSession() : bool {
+            session_save_path();
+            return session_start();
+        }
+        public static function takeID(string $email) : int {
+            $conn = BaseFunction::DBconnection();
+            return $conn->query("SELECT DISTINCT ID_utente FROM utenti WHERE email = $email")->fetch_assoc();
+        }
     }
-    public static function takeID(string $email) : int {
-        $conn = BaseFunction::DBconnection();
-        return $conn->query("SELECT DISTINCT ID_utente FROM utenti WHERE email = $email")->fetch_assoc();
-    }
-}
-
 ?>
